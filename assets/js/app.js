332e0513fb935904be64236c83b318343b244f3a
Anh Le <eric.anhlhv@gmail.com>
2020-12-15 01:08:04 +0700

    fix responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 //= require vendor/jquery
 //= require vendor/jquery.form
-//= require logisti/plugins
-//= require logisti/main
+//= require vendor/bootstrap
 //= require vendor/sweetalert2
 //= require script
